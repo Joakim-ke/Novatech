cbf6668e540950ac35e5be4c2b46c7a04b07ab63
Joakim <jkimhirwa12@gmail.com>
2026-04-27 06:07:09 +0300

    login

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ if (slider && prevButton && nextButton) {
     slider.scrollBy({ left: slideAmount, behavior: 'smooth' });
   });
 }
+
+
+let btn = document.getElementById("openLogin");
+let box = document.getElementById("loginBox");
+let overlay = document.getElementById("overlay");
+
+btn.onclick = () =>{
+  box.classList.add("show") 
+  overlay.classList.add("show")
+};
+
+overlay.onclick = () =>{
+  box.classList.remove("show") 
+  overlay.classList.remove("show")
+};
